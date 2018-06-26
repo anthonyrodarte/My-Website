d6c3fc153e08c415afc7e34d5252b0d5498aa5f9
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-06-25 18:48:47 -0700

    Used React to render landing. #1

---
+++ client/components/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function App() {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col intro">
+          <h1 className="display-3">Anthony Rodarte</h1>
+          <h3>Web Developer</h3>
+          <button href="https://www.linkedin.com/in/anthony-rodarte-99abb1155/" type="button" className="btn btn-light">LINKEDIN</button>
+          <button href="https://github.com/occs/eleven" type="button" className="btn btn-light">GITHUB</button>
+        </div>
+      </div>
+    </div>
+  )
+}
