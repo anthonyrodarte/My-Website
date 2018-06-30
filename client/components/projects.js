71dffc6126f94794185cf0666cd210ac1ecc62d1
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-06-29 19:49:41 -0700

    Created Projects section. #4

--- client/components/projects.js
+++ client/components/projects.js
@@ -4,6 +4,9 @@ export default function Projects() {
   return (
     <div className="container text-center">
       <div className="row">
+        <div className="col">
+          <h2>Projects</h2>
+        </div>
       </div>
     </div>
   )
--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Landing from './components/landing'
+import Projects from './components/projects'
 
 ReactDOM.render(<Landing />, document.querySelector('#landing'))
+ReactDOM.render(<Projects />, document.querySelector('#projects'))
